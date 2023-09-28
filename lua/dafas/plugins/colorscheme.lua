408f8c30e7060d1e578179756f7d18ffe6e6c03b
dafascoder <shirwa256@gmail.com>
2023-09-28 00:55:25 -0400

    added treesitter

--- lua/dafas/plugins/colorscheme.lua
+++ lua/dafas/plugins/colorscheme.lua
@@ -1,10 +1,10 @@
 return {
-	"projekt0n/github-nvim-theme",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		require("github-theme").setup({})
-
-		vim.cmd([[colorscheme github_dark_dimmed]])
-	end,
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			vim.cmd([[colorscheme catppuccin]])
+		end,
+	},
 }
